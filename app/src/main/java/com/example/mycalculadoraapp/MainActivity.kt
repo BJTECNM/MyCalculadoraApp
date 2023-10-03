3270d79c09fb27dac5d9ac1a228f937c15968e7f
BJ <192t0220@itsm.edu.mx>
2023-10-03 16:35:27 -0600

    -git commit

--- app/src/main/java/com/example/mycalculadoraapp/MainActivity.kt
+++ app/src/main/java/com/example/mycalculadoraapp/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.mycalculadoraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.mycalculadoraapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel : MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        binding.btnSuma.setOnClickListener {
+
+        }
     }
 }
