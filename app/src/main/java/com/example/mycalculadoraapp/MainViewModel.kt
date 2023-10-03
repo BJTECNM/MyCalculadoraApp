2d50d2d2b67d20be0eef5f15b039ce75b799bc70
BJ <192t0220@itsm.edu.mx>
2023-10-03 17:08:19 -0600

    -git commit

--- app/src/main/java/com/example/mycalculadoraapp/MainViewModel.kt
+++ app/src/main/java/com/example/mycalculadoraapp/MainViewModel.kt
@@ -3,7 +3,23 @@ package com.example.mycalculadoraapp
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var num1 = 0
-    var num2 = 0
+    //var num1 = 0
+    //var num2 = 0
+    var resultado = 0
 
+    fun suma(num1: Int, num2: Int){
+        resultado = num1+num2
+    }
+
+    fun resta(num1: Int, num2: Int){
+        resultado = num1-num2
+    }
+
+    fun multiplicacion(num1: Int, num2: Int){
+        resultado = num1*num2
+    }
+
+    fun division(num1: Int, num2: Int){
+        resultado = num1/num2
+    }
 }
